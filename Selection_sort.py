e74b17fef7fea7f629b5dff53ea25aad112b94de
Heather Doucet <heatherredoucet@gmail.com>
2016-07-12 14:57:47 -0500

    adding Selection_sort.py

---
+++ Selection_sort.py
@@ -0,0 +1,19 @@
+import random
+myList = random.sample(xrange(10000), 100)
+print myList
+
+def Selection_sort(myList):
+    for i in range(len(myList)):
+        MaxPosition = i
+        for location in range(i+1, len(myList) ):
+            if myList[location]>myList[MaxPosition]:
+                MaxPosition = location
+
+def swap_indices(myList, index1, index2):
+    temp = myList[index1]
+    myList[index1] = myList[index2]
+    myList[index2] = temp
+
+    return myList
+new_list = Selection_sort(myList)
+print new_list
